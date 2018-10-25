eeb7a4bb29551f5e30b4d54855f8e5d392aa8710
raajeshmani <raajeshmani19@gmail.com>
2018-10-25 14:03:58 +0530

    Remove old files

--- homeController.js
+++
@@ -1,8 +0,0 @@
-//homeController.js
-
-var app = angular.module('myApp');
-
-app.controller('HomeController',
-	function($scope, $rootScope, $stateParams, $state, LoginService) {
-		$scope.user = $rootScope.userName;
-	});
